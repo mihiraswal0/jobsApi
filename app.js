df2e47676b8414c803dafa81bffb14e571ffc2a0
Mihir Aswal <montyaswal0@gmail.com>
2023-09-13 09:19:37 +0530

    setup backend

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 require('dotenv').config();
-const expess=require('express');
+require('./db/connect.js');
+const express=require('express');
 const app=express();
 const bodyParser=require('body-parser');
 const errorHandler=require('./middleware/errorHandler.js');
 const notFound=require('./middleware/notFound.js');
+const tasks=require('./routes/tasks.js');
 const port=process.env.PORT ||5000;
 
 
 app.use(bodyParser.json());
 
-app.use(notFound);
+
+app.use('/api/v1/products',tasks);
+
 app.use(errorHandler);
+app.use(notFound);
 app.listen(port,()=>{
     console.log('listening on port'+port);
 })
--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,4 +1,4 @@
-const error=(err,req,res,next)=>{
+const errorHandler=(err,req,res,next)=>{
     return res.status(err.status).json(err.message);
 }
 module.exports =errorHandler;
